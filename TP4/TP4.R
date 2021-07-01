d0a2f26a9ecc6ba1956ce86519bac0a6c401c137
ILitman <ignaciolitmanovich@gmail.com>
2021-07-01 18:21:04 -0300

    Update TP4.R

--- TP4/TP4.R
+++ TP4/TP4.R
@@ -7,8 +7,9 @@ columna3 <- createFolds(t(dataSet[,10]), k=3) # Dividimos el data set en 5 grupo
 grupoEntrenamiento <- dataSet[columna3[[3]], ] # obtenemos los valores del dataset del grupo 3
 entrenamiento <- dataSet[setdiff(seq(1:dim(dataSet)[1]), columna3[[3]]), ] 
 entrenamientoFit <- rpart(X1.1~. , data = entrenamiento, parms = list(split = "gini"))
+poda <- prune(entrenamientoFit, cp=0.1)
 summary(entrenamientoFit)
-fancyRpartPlot(entrenamientoFit, caption=NULL)
+fancyRpartPlot(poda, caption=NULL)
 dataSet
 
 # X13.64    X0.00    X0.06    X1.10    X4.49    X8.75 X1.52101 
