b4682e5dbe203f8f25aab2a3abb2c5e1e99573a5
Damián Ariel Marotte <drd00m@msn.com>
2021-07-03 04:26:06 -0300

    Modelos con multiples semillas.

--- TP4/TP4.R
+++ TP4/TP4.R
@@ -1,40 +1,97 @@
-library(caret)
-library(rpart)
-library(rattle)
-library(rpart.plot)
-library(e1071)
-dataSetOriginal <- read.csv("E:\\repos\\LCC-IIA\\TP4\\glass.data") # esto estamos seguro 
-dataSet <- dataSetOriginal[,-1]
+library(caret)      # createDataPartition
+library(rpart)      # rpart
+library(rpart.plot) # rpart.plot
+library(e1071)      # confusionMatrix
 
-colnames(dataSet)[1] <- "R.I"
-colnames(dataSet)[2] <- "Sodio"
-colnames(dataSet)[3] <- "Magnesio"
-colnames(dataSet)[4] <- "Aluminio" #importante para la clasificacion
-colnames(dataSet)[5] <- "Silicio"
-colnames(dataSet)[6] <- "Potasio"
-colnames(dataSet)[7] <- "Calcio"
-colnames(dataSet)[8] <- "Bario"
-colnames(dataSet)[9] <- "Hierro"
+# Leemos el dataset original
+dataSetOriginal <- read.csv("glass.data")
+
+# Eliminamos la columna de los indices.
+dataSet <- dataSetOriginal[, -1]
+
+# Ponemos nombre a las variables
+colnames(dataSet)[1]  <- "R.I"
+colnames(dataSet)[2]  <- "Sodio"
+colnames(dataSet)[3]  <- "Magnesio"
+colnames(dataSet)[4]  <- "Aluminio"
+colnames(dataSet)[5]  <- "Silicio"
+colnames(dataSet)[6]  <- "Potasio"
+colnames(dataSet)[7]  <- "Calcio"
+colnames(dataSet)[8]  <- "Bario"
+colnames(dataSet)[9]  <- "Hierro"
 colnames(dataSet)[10] <- "Clase"
 
-dataSet[,"Clase"] <- as.factor(dataSet[,"Clase"]) #
-clasificacion <- createFolds(t(dataSet[,10]), k=5, ) # Dividimos el data set en 5 grupos para obter el 80% de entrenamiento.
+# Convertimos a factor la columna de las clases.
+dataSet[, "Clase"] <- as.factor(dataSet[, "Clase"])
 
-#grupoEntrenamiento <- dataSet[clasificacion[[3]], ] # obtenemos los valores del dataset del grupo 3
+# Contadores para elegir la mejor metrica
+ginni = 0
+information = 0
+sumaGinni = 0
+sumaInformation = 0
+semillas = 50
 
-entrenamiento <- dataSet[setdiff(seq(1:dim(dataSet)[1]), clasificacion[[3]]), ] 
-entrenamientoFit <- rpart(Clase~Aluminio+Magnesio+Bario+R.I+Sodio, data = entrenamiento, parms = list(split = "information"))
-prediccion <- predict(entrenamientoFit, dataSet[clasificacion[[3]], -10], type="class")
-confusionMatrix(prediccion, dataSet[clasificacion[[3]],10])
-fancyRpartPlot(entrenamientoFit, caption=NULL)
-plot(dataSet[,4], dataSet[,3], type="p", col=dataSet[,"Clase"])
+for (seed in seq(0, semillas)) {
+    # Establecemos una semilla.
+    set.seed(seed)
 
+    # Dividimos el dataset en datos de entrenamiento y prueba.
+    indices <- createDataPartition(dataSet[, "Clase"], p = .8, list = FALSE)
+    entrenamiento <- dataSet[indices,]
+    prueba <- dataSet[-indices,]
 
-poda <- prune(entrenamientoFit, cp=0.01)
-prediccionPoda <- predict(poda, dataSet[clasificacion[[3]], -10], type="class")
-confusionMatrix(prediccionPoda, dataSet[clasificacion[[3]],10])
+    # Creamos modelos a partir de los datos de entrenamiento.
+    informationFit <- rpart(Clase ~ ., # Aluminio + Magnesio + Bario + R.I + Sodio,
+                            data = entrenamiento, parms = list(split = "information"))
+    ginniFit <- rpart(Clase ~ ., # Aluminio + Magnesio + Bario + R.I + Sodio,
+                      data = entrenamiento, parms = list(split = "gini"))
 
-summary(entrenamientoFit)
-dataSet
+    # Podamos los arboles
+    podaI <- prune(informationFit, cp = 0.01)
+    podaG <- prune(ginniFit, cp = 0.01)
 
-# X13.64    X0.00    X0.06    X1.10    X4.49    X8.75 X1.52101 
+    # Probamos los modelos en los datos de prueba.
+    prediccionI <- predict(informationFit, prueba, type = "class")
+    prediccionG <- predict(ginniFit, prueba, type = "class")
+    prediccionPodaI <- predict(podaI, prueba, type = "class")
+    prediccionPodaG <- predict(podaG, prueba, type = "class")
+
+    # Calculamos estadisticas.
+    matrizI <- confusionMatrix(prediccionI, prueba[, "Clase"])
+    matrizG <- confusionMatrix(prediccionG, prueba[, "Clase"])
+    matrizPodaI <- confusionMatrix(prediccionPodaI, prueba[, "Clase"])
+    matrizPodaG <- confusionMatrix(prediccionPodaG, prueba[, "Clase"])
+
+    if (matrizPodaI$overall["Accuracy"] > matrizI$overall["Accuracy"]) {
+        cat("ATENCION: Poda mejor que information.\n")
+    }
+    if (matrizPodaG$overall["Accuracy"] > matrizG$overall["Accuracy"]) {
+        cat("ATENCION: Poda mejor que ginni\n")
+    }
+
+    cat("[", seed, "] ")
+    if (matrizG$overall["Accuracy"] >= matrizI$overall["Accuracy"]) {
+        cat("Ginni Accuracy:\t\t", matrizG$overall["Accuracy"], "\n")
+        ginni <- ginni + 1
+        sumaGinni <- sumaGinni + matrizG$overall["Accuracy"]
+    } else {
+        cat("Information Accuracy:\t", matrizI$overall["Accuracy"], "\n")
+        information <- information + 1
+        sumaInformation <- sumaInformation + matrizI$overall["Accuracy"]
+    }
+
+    # Dibujamos el arbol
+    # rpart.plot(fit)
+}
+
+# Decidimos la mejor metrica
+promedioG = sumaGinni / ginni
+promedioI = sumaInformation / information
+cat("\n")
+if (promedioG >= promedioI) {
+    cat("Mejor metrica: ginni.\n")
+    cat("Accuracy promedio:", promedioG, "\n")
+} else {
+    cat("Mejor metrica: information.\n")
+    cat("Accuracy promedio:", promedioI, "\n")
+}
